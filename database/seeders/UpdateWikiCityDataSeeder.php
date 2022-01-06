6c625de5185609153c00e307186621c97892b5f4
Prashant Khanderia <prashant.k@tridhya.com>
2022-01-06 16:46:30 +0530

    Change in city condtion

--- database/seeders/UpdateWikiCityDataSeeder.php
+++ database/seeders/UpdateWikiCityDataSeeder.php
@@ -28,10 +28,10 @@ class UpdateWikiCityDataSeeder extends Seeder
         DB::table('city')->where('wikipedia_city_name', 'Artern/Unstrut')->update(['wikipedia_city_name' => 'Artern']);
         DB::table('city')->where('wikipedia_city_name', 'Bischofsheim an der Rhön')->update(['wikipedia_city_name' => 'Bischofsheim_in_der_Rhön']);
         DB::table('city')->where('wikipedia_city_name', 'Eisleben')->update(['wikipedia_city_name' => 'Lutherstadt_Eisleben']);
-        DB::table('city')->where('wikipedia_city_name', ' Königsee-Rottenbach')->update(['wikipedia_city_name' => 'Königsee']);
+        DB::table('city')->where('wikipedia_city_name', 'Königsee-Rottenbach')->update(['wikipedia_city_name' => 'Königsee']);
         DB::table('city')->where('wikipedia_city_name', 'Oberweißbach/Thür. Wald')->update(['wikipedia_city_name' => 'Oberweißbach/Thüringer_Wald']);
-        DB::table('city')->where('wikipedia_city_name', ' Rothenburg/O.L.')->update(['wikipedia_city_name' => 'Rothenburg/Oberlausitz']);
-        DB::table('city')->where('wikipedia_city_name', ' Rottenburg a.d.Laaber')->update(['wikipedia_city_name' => 'Rottenburg an der Laaber']);
+        DB::table('city')->where('wikipedia_city_name', 'Rothenburg/O.L.')->update(['wikipedia_city_name' => 'Rothenburg/Oberlausitz']);
+        DB::table('city')->where('wikipedia_city_name', 'Rottenburg a.d.Laaber')->update(['wikipedia_city_name' => 'Rottenburg an der Laaber']);
         DB::table('city')->where('wikipedia_city_name', 'Stollberg/Erzgeb.')->update(['wikipedia_city_name' => 'Stollberg/Erzgebirge']);
         DB::table('city')->where('wikipedia_city_name', 'Tessin')->update(['wikipedia_city_name' => 'Kanton Tessin']);
         DB::table('city')->where('wikipedia_city_name', 'Weißwasser/O.L.')->update(['wikipedia_city_name' => 'Weißwasser/Oberlausitz']);
